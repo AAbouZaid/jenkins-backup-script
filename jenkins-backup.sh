654dd25b4ea1fd0b21eda17f1c77bfead642a807
Ahmed M. AbouZaid <AAbouZaid@users.noreply.github.com>
2016-03-10 12:22:45 +0100

    Tracking config with git instead archiving them.
    Now this script push changes to git repo instead make a full tar archive.
    Repo initialize is done via configuration management too (e.g. Ansible or Saltstack).

--- jenkins-backup.sh
+++ jenkins-backup.sh
@@ -2,24 +2,22 @@
 
 ##################################################################################
 function usage(){
-  echo "usage: $(basename $0) /path/to/jenkins_home archive.tar.gz"
+  echo "usage: $(basename $0) /path/to/jenkins_home"
 }
 ##################################################################################
 
 readonly JENKINS_HOME=$1
-readonly DEST_FILE=$2
+readonly GIT_SERVICE="bitbucket"
 readonly CUR_DIR=$(cd $(dirname ${BASH_SOURCE:-$0}); pwd)
-readonly TMP_DIR="$CUR_DIR/tmp"
-readonly ARC_NAME="jenkins-backup"
+readonly TMP_DIR="$CUR_DIR/backup"
+readonly ARC_NAME="jenkins-$GIT_SERVICE-backup"
 readonly ARC_DIR="$TMP_DIR/$ARC_NAME"
-readonly TMP_TAR_NAME="$TMP_DIR/archive.tar.gz"
 
-if [ -z "$JENKINS_HOME" -o -z "$DEST_FILE" ] ; then
+if [ -z "$JENKINS_HOME" ] ; then
   usage >&2
   exit 1
 fi
 
-rm -rf "$ARC_DIR" "$TMP_TAR_NAME"
 mkdir -p "$ARC_DIR/"{plugins,jobs,users,secrets}
 
 cp "$JENKINS_HOME/"*.xml "$ARC_DIR"
@@ -45,13 +43,12 @@ if [ -d "$JENKINS_HOME/jobs/" ] ; then
     mkdir -p "$ARC_DIR/jobs/$job_name/"
     find "$JENKINS_HOME/jobs/$job_name/" -maxdepth 1 -name "*.xml" | xargs -I {} cp {} "$ARC_DIR/jobs/$job_name/"
   done
-  cd -
+  cd $JENKINS_HOME
 fi
 
 cd "$TMP_DIR"
-tar -czvf "$TMP_TAR_NAME" "$ARC_NAME/"*
-cd -
-mv -f "$TMP_TAR_NAME" "$DEST_FILE"
-rm -rf "$ARC_DIR"
+git add -A "$ARC_NAME/"
+git commit -m "Jenkins backup at $(date +%Y%m%d_%H%M)"
+git push
 
 exit 0
